e941c6cef72021a0a3f8d89951773906ae76b09b
yantijin <yantijin@163.com>
2022-09-29 10:55:04 +0800

    add main.go

---
+++ server/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"GoLog/commen"
+	"GoLog/core"
+	"GoLog/initialize"
+	"fmt"
+)
+
+func main() {
+	// 首先读取配置,之后初始化全局变量,最后开启服务
+	commen.GVA_VP = core.Viper() // 全局配置读取到commen.GVA_CONFIG,并实时监控文件变化
+	fmt.Print(commen.GVA_CONFIG)
+	commen.GVA_LOG = core.Zap()
+	// 初始化数据库
+	commen.GVA_DB = initialize.InitializeDb()
+
+	if commen.GVA_DB != nil {
+		commen.GVA_LOG.Info("连接数据库成功")
+		// 初始化表格
+		initialize.MigrateTables(commen.GVA_DB)
+		db, _ := commen.GVA_DB.DB()
+		defer db.Close()
+	}
+
+}
