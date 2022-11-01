238d71cd1873e4ad61c7b63038dec12bd1085f49
yantijin <yantijin@163.com>
2022-11-01 22:02:14 +0800

    add pinia 状态管理, mitt 组件通信, axios 请求/响应拦截器

---
+++ web/src/pinia/modules/user.ts
@@ -0,0 +1,39 @@
+/*
+  主要是用户相关的状态操作；
+  token设置，接收，管理；
+  用户信息设置接收管理；
+  用户登录，登出
+*/
+
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+interface UserInfo {
+  uuid: string;
+  nickname: string;
+  avatar: string;
+  id: string;
+}
+
+export const useUserStore = defineStore("user", () => {
+  const userInfo = ref<UserInfo>({
+    uuid: "",
+    nickname: "",
+    avatar: "",
+    id: "",
+  });
+
+  const token = ref(window.localStorage.getItem("token") || "");
+  const setUserInfo = (val: UserInfo) => {
+    userInfo.value = val;
+  };
+  const setToken = (val: string) => {
+    token.value = val;
+  };
+  return {
+    userInfo,
+    token,
+    setUserInfo,
+    setToken,
+  };
+});
---
+++ web/src/utils/request.ts
@@ -0,0 +1,99 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import { emitter } from "@/utils/bus";
+import { useUserStore } from "@/pinia/modules/user";
+import { ElMessage, ElMessageBox } from "element-plus";
+
+const service = axios.create({
+  baseURL: import.meta.url,
+  timeout: 99999,
+});
+
+let activeAxios = 0;
+let timer: number;
+const showLoading = () => {
+  activeAxios++;
+  if (timer) {
+    clearTimeout(timer);
+  }
+  timer = setTimeout(() => {
+    if (activeAxios > 0) {
+      emitter.emit("showLoading");
+    }
+  });
+};
+// 关闭的loading界面
+const closeLoading = () => {
+  activeAxios--;
+  if (activeAxios <= 0) {
+    emitter.emit("closeLoading");
+  }
+};
+
+// 请求拦截器
+service.interceptors.request.use(
+  (config: AxiosRequestConfig) => {
+    // if (!config.doNotShowLoading) {
+    showLoading();
+    // }
+    const userStore = useUserStore();
+    config.headers = {
+      "Content-Type": "application/json",
+      "x-token": userStore.token,
+      "x-user-id": userStore.userInfo.id,
+      ...config.headers,
+    };
+    return config;
+  },
+  (error: any) => {
+    closeLoading();
+    ElMessage({
+      showClose: true,
+      message: error,
+      type: "error",
+    });
+    return error;
+  }
+);
+
+// 响应拦截器
+service.interceptors.response.use(
+  (response: AxiosResponse) => {
+    const userStore = useUserStore();
+    if (response.data.data === 0 || response.headers.success === "true") {
+      if (response.headers.msg) {
+        response.data.msg = decodeURI(response.headers.msg);
+      }
+    } else {
+      ElMessage({
+        showClose: true,
+        message: response.data.msg || decodeURI(response.headers.msg!),
+        type: "error",
+      });
+      if (response.data.data && response.data.data.reload) {
+        userStore.token = "";
+        localStorage.clear();
+        // TODO: 这里需要使用router跳转到登录界面
+      }
+    }
+    return response;
+  },
+  (error: any) => {
+    closeLoading();
+    if (!error.response) {
+      ElMessageBox.confirm(
+        `
+      <p>检测到请求错误</p>
+      <p>${error}</p>
+      `,
+        "请求错误",
+        {
+          dangerouslyUseHTMLString: true,
+          distinguishCancelAndClose: true,
+          confirmButtonText: "请稍后重试",
+          cancelButtonText: "取消",
+        }
+      );
+    }
+    return error;
+  }
+);
