fb51da1106833ea906508d33d724d75e57802c60
yantijin <yantijin@163.com>
2022-11-20 16:47:42 +0800

    添加前端代理，修改环境配置文件，添加vue-router依赖

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,7 +4,8 @@ import App from "./App.vue";
 import { store } from "@/pinia";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import router from "./router";
 
 const app = createApp(App);
-app.use(ElementPlus).use(store);
+app.use(ElementPlus).use(store).use(router);
 app.mount("#app");
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,19 +1,37 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import eslintPlugin from 'vite-plugin-eslint';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import eslintPlugin from "vite-plugin-eslint";
+import { resolve } from "path";
+import * as dotenv from "dotenv";
+import { readFileSync } from "fs";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), eslintPlugin()],
-  server: {
-    host: 'localhost',
-    port: 8080,
-    open: true,
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'), // 设置@代表路径 ./src
+export default defineConfig(({ command, mode }) => {
+  const file = `.env.${mode}`;
+  const envConfig = dotenv.parse(readFileSync(file));
+  for (const k in envConfig) {
+    process.env[k] = envConfig[k];
+  }
+
+  return {
+    plugins: [vue(), eslintPlugin()],
+    server: {
+      host: "localhost",
+      port: parseInt(process.env.VITE_CLI_PORT),
+      open: true,
+      proxy: {
+        [process.env.VITE_BASE_API]: {
+          target: `${process.env.VITE_BASE_PATH}:${process.env.VITE_SERVER_PORT}`,
+          changeOrigin: true, // changes the origin of the host header to the target url
+          rewrite: path =>
+            path.replace(new RegExp("^" + process.env.VITE_BASE_API), ""),
+        },
+      },
     },
-  },
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"), // 设置@代表路径 ./src
+      },
+    },
+  };
 });
