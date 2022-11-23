8919a16c5025b2f2bff1b4ec997a70fb56860aa3
yantijin <yantijin@163.com>
2022-11-23 13:27:29 +0800

    添加文章详情card，评论card

---
+++ web/src/components/ArticleViewCard.vue
@@ -0,0 +1,72 @@
+<template>
+  <el-card class="article-view-card">
+    <template #header>
+      <h2 id="title">{{ article.Title }}</h2>
+      <div class="article-view-meta">
+        <span
+          >由
+          <router-link
+            :to="'/user/' + article.UserInfo.ID"
+            class="article-author"
+            >{{ article.UserInfo.NickName }}</router-link
+          >
+        </span>
+        发布于
+        <time>{{ article.CreateTime }}</time>
+      </div>
+    </template>
+    <div v-html="article.Content" class="content"></div>
+  </el-card>
+</template>
+
+<script lang="ts" setup>
+// const article = async() => {
+//   await
+// }
+
+import { ArticleResponse } from "@/model/response";
+import { RouterLink } from "vue-router";
+
+const article: ArticleResponse = {
+  ArticleId: 1,
+  Title: "测试",
+  Content: "<a href='https://whiteyan.top'>小白的个人博客</a>",
+  ViewCount: 1,
+  CreateTime: 100,
+  UserInfo: { ID: 1, NickName: "小白", Avatar: "", CreateTime: 0 },
+  LikeCount: 4,
+  CommentCount: 10,
+  CommnetTime: 30,
+};
+</script>
+
+<style>
+.article-view-card {
+  width: 70vw;
+  margin-bottom: 0px;
+}
+
+#title {
+  text-align: left;
+}
+
+.article-view-meta {
+  display: flex;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  padding: 0 6px 0 0;
+  align-items: center;
+  font-size: 13px;
+}
+
+.article-author {
+  font-weight: bold;
+  padding: 0 3px;
+  color: black;
+}
+
+.content {
+  text-align: left;
+  font-size: 16px;
+}
+</style>
---
+++ web/src/components/Comment.vue
@@ -0,0 +1,44 @@
+<!-- 这里假设comment也能够输入富文本来编辑 -->
+<template>
+  <div class="comment-container">
+    <div>
+      <MdEditorVue
+        placeholder="请输入评论内容"
+        theme="classic"
+        height="auto"
+      ></MdEditorVue>
+    </div>
+    <div class="btn">
+      <el-button type="primary" @click="submitComment">提交</el-button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import MdEditorVue from "@/components/MdEditor.vue";
+
+interface commentProps {
+  entityType: string;
+  entityId: number;
+}
+
+const props = withDefaults(defineProps<commentProps>(), {
+  entityType: "article",
+  entityId: 0,
+});
+
+const submitComment = () => {
+  console.log("hhh");
+};
+</script>
+
+<style>
+.btn {
+  text-align: right;
+  padding-top: 6px;
+}
+
+.comment-container {
+  border: 2px solid grey;
+}
+</style>
